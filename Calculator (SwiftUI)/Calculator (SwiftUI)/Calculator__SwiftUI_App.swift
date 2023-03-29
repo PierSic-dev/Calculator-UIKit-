a2ee0bd80a0902544a17070769a0ec46cda4ccd2
PierSic-dev <113537165+PierSic-dev@users.noreply.github.com>
2023-03-29 11:27:44 +0200

    Initial commit

---
+++ Calculator (SwiftUI)/Calculator (SwiftUI)/Calculator__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator__SwiftUI_App.swift
+//  Calculator (SwiftUI)
+//
+//  Created by Pierpaolo Siciliano on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Calculator__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
